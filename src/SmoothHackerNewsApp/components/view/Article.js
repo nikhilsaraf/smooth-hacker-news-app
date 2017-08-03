edd1518d20dac1383f30302166221996ae5e1594
Nikhil <nikhilsaraf89@gmail.com>
2017-08-02 23:37:43 -0700

    fix web view scaling issue

--- src/SmoothHackerNewsApp/components/view/Article.js
+++ src/SmoothHackerNewsApp/components/view/Article.js
@@ -14,6 +14,7 @@ class Article extends React.Component {
 		const { params } = this.props.navigation.state;
 		return <WebView
 			source = { { uri: params.url } }
+            scalesPageToFit = {true}
 		/>;
 	}
 }
