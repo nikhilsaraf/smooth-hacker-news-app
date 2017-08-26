c5d7576eb3cc215ee0da36f44df56230db240f83
Nikhil <nikhilsaraf89@gmail.com>
2017-08-26 14:47:44 -0700

    Added Rate app button, closes #21

--- src/SmoothHackerNewsApp/components/view/ReaderView.js
+++ src/SmoothHackerNewsApp/components/view/ReaderView.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ActivityIndicator, ScrollView } from 'react-native';
+import { View, TouchableOpacity, Text, ActivityIndicator, ScrollView, Linking } from 'react-native';
 import { Cell, TableView } from 'react-native-tableview-simple';
 import PullToRefresh from 'react-native-pull-to-refresh';
 
@@ -64,6 +64,10 @@ class ReaderView extends React.Component {
     return new Promise(this._loadData.bind(this));
   }
 
+  _onPressRateApp() {
+    Linking.openURL("https://shn.app.link/rate-story-list");
+  }
+
   render() {
     if (!this.state.dataList) {
       return (<ActivityIndicator
@@ -76,6 +80,22 @@ class ReaderView extends React.Component {
         <TableView>
         {this._generateRows(this.state.dataList)}
         </TableView>
+        <View style = {{ backgroundColor: "#1194e0", height: 40 }}>
+          <TouchableOpacity onPress = { this._onPressRateApp.bind(this) } style = {{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <Text numberOfLines={1} style={{
+              fontSize: 18,
+              fontWeight: "500",
+              color: "#fff",
+              textAlign: "center"
+            }}>
+            Rate App
+            </Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>);
 
     if (this.props.canRefresh) {
